c1677314b0fc44082e8ec663e5a29fbc0f01ddc6
JakeCooper <jake.elijah.cooper@gmail.com>
2015-06-30 17:51:42 -0700

    Add side on right for search tray.

--- Frontend/js/sidebar.js
+++ Frontend/js/sidebar.js
@@ -3,4 +3,8 @@ $(document).ready(function() {
         name: 'sidr-left',
         side: 'left' // By default
     });
+    $('#right-menu').sidr({
+        name: 'sidr-right',
+        side: 'right' // By default
+    });
 });
