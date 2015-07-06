d13ebd7323823d1ea34041db3f4a91debba12796
andrei <andreirtaylor@gmail.com>
2015-07-06 09:49:35 -0700

    Move socket logic into separate file

--- Server/bin/www.js
+++ Server/bin/www.js
@@ -33,20 +33,7 @@ server.on('listening', onListening);
 // sever, the sockets nedd to be bound to ther server after connection
 // basically this means you cant add in the socket logic until after
 // this point.
-var io = require('socket.io')(server);
-var passport = require("passport");
-var config = require('config');
-
-// authenticate sockets
-io.use(function (socket, next) {
-    // requires the application to be initialized
-    // this is not stored in the config file
-    config.sessionMiddleware(socket.handshake, {}, next);
-});
-
-// will only continue on if the user is logged in
-io.use(require("../sockets/authorizeSockets.js"));
-(require("../sockets/socketLogic.js")(io));
+require('../sockets/sockets.js')(server);
 
 /**
  * Normalize a port into a number, string, or false.
