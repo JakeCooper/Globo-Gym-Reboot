2e451b381416ea4663d431ed23ab7411181f81ef
colemcginn <colemcginn@hotmail.com>
2015-07-08 12:36:40 -0700

    Modal fix

--- Frontend/js/controllers/calendarControllers.js
+++ Frontend/js/controllers/calendarControllers.js
@@ -1,7 +1,7 @@
 var module = angular.module("calendarControllers", []);
 
 module.controller('calendarController', ['$scope', '$compile', 'uiCalendarConfig', 'socket',
-    function ( $scope, $compile, uiCalendarConfig, socket) {
+    function ( $scope, $compile, uiCalendarConfig, socket, $modal) {
     var date = new Date();
     var d = date.getDate();
     var m = date.getMonth();
@@ -22,6 +22,14 @@ module.controller('calendarController', ['$scope', '$compile', 'uiCalendarConfig
     };
 
     $scope.eventSources = [$scope.reservations,$scope.events];
+    $scope.$on('pushEvent', function(title, start, end){
+        $scope.events.push({
+            title: title,
+            start: start,
+            end: end,
+        });
+    });
+
     /* add custom event*/
     $scope.addEvent = function() {
       $scope.events.push({
@@ -45,8 +53,7 @@ module.controller('calendarController', ['$scope', '$compile', 'uiCalendarConfig
             dayClick: function(){
                 var modalInstance = $modal.open({
                   animation: $scope.animationsEnabled,
-                  template: '<table> <tr> <td> <input type="checkbox" name="dlg_allday" id="dlg_allday" value="1" checked="{$IN.allday}" />allday </td> </tr> <tr> <td> Start: </td> <td align="left"> <input type="text" id="event_start_date" name="event_start_date" value="{$IN.event_start_date}" style="width:120px;" /> <input type="text" id="event_start_time" name="event_start_time" value="{$IN.event_start_time}" style="width:80px;" /> </td> </tr> <tr> <td> End: </td> <td align="left"> <input type="text" id="event_end_date" name="event_end_date" value="{$IN.event_end_date}" style="width:120px;" /> <input type="text" id="event_end_time" name="event_end_time" value="{$IN.event_end_time}" style="width:80px;"/> </td> </tr> <tr> <td> Subject: </td> <td align="left"> <input type="text" id="event_subject" name="event_subject" maxlength="255" style="width:460px;"/> </td> </tr> </table> <center><textarea name="event_description" id="event_description" rows="9" style="width:520px;"></textarea></center> <center> <button type="button" id="dlgcommit">New event</button> <button type="button" id="dlgcancel">Cancel</button> </center> </form>',
-                  // '<div class="modal-header"><h3 class="modal-title">I m a modal!</h3></div>',
+                  templateUrl: 'partials/addmodal.html',
                   controller: 'modalInstanceController',
                   size: 'sm',
                 });
@@ -59,26 +66,48 @@ module.controller('calendarController', ['$scope', '$compile', 'uiCalendarConfig
 ]);
 module.controller('modalController', function($scope,$modal){
     $scope.animationsEnabled = true;
-
+    $scope.createEvent = function ($scope) {
+        var startTime = $scope.startTime;
+        var endTime = $scope.endTime;
+    };
     $scope.open = function (size) {
 
         var modalInstance = $modal.open({
           animation: $scope.animationsEnabled,
-          template: '<table> <tr> <td> <input type="checkbox" name="dlg_allday" id="dlg_allday" value="1" checked="{$IN.allday}" />allday </td> </tr> <tr> <td> Start: </td> <td align="left"> <input type="text" id="event_start_date" name="event_start_date" value="{$IN.event_start_date}" style="width:120px;" /> <input type="text" id="event_start_time" name="event_start_time" value="{$IN.event_start_time}" style="width:80px;" /> </td> </tr> <tr> <td> End: </td> <td align="left"> <input type="text" id="event_end_date" name="event_end_date" value="{$IN.event_end_date}" style="width:120px;" /> <input type="text" id="event_end_time" name="event_end_time" value="{$IN.event_end_time}" style="width:80px;"/> </td> </tr> <tr> <td> Subject: </td> <td align="left"> <input type="text" id="event_subject" name="event_subject" maxlength="255" style="width:460px;"/> </td> </tr> </table> <center><textarea name="event_description" id="event_description" rows="9" style="width:520px;"></textarea></center> <center> <button type="button" id="dlgcommit">New event</button> <button type="button" id="dlgcancel">Cancel</button> </center> </form>',
-          // '<div class="modal-header"><h3 class="modal-title">I m a modal!</h3></div>',
+          templateUrl: 'partials/addmodal.html',
           controller: 'modalInstanceController',
           size: size,
         });
     };
+
 });
 
 module.controller('modalInstanceController', function($scope, $modalInstance){
+    var startTime =  new Date();
+    var endTime = new Date();
+
+
     $scope.ok = function () {
-        $modalInstance.close($scope.selected.item);
+        var title = $scope.eventTitle;
+        startTime = $scope.startTime;
+        endTime = $scope.endTime;
+        $scope.$emit('pushEvent', title, startTime, endTime);
+
+        $modalInstance.close();
+
     };
 
     $scope.cancel = function () {
         $modalInstance.dismiss('cancel');
     };
+
+});
+module.controller('timepickerController', function ($scope, $log) {
+  $scope.startTime = new Date();
+  $scope.startTime.setMinutes(0);
+  $scope.endTime = new Date();
+  $scope.endTime.setMinutes(0);
+  $scope.hstep = 1;
+  $scope.mstep = 30;
 
 });
