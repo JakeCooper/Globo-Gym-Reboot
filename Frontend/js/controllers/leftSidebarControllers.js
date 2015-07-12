788a3388c05cc64abdae8c74c33b518599a6acc5
colemcginn <colemcginn@hotmail.com>
2015-07-12 02:18:35 -0700

    merge unconflicted

--- Frontend/js/controllers/leftSidebarControllers.js
+++ Frontend/js/controllers/leftSidebarControllers.js
@@ -5,11 +5,6 @@ module.controller('leftSidebarController', function($scope, socket){
     socket.on("profileInfo", function(data){
 
        $scope.username = data.username;
-<<<<<<< HEAD
-=======
-       console.log($scope.username);
-       console.log($scope.firstName);
->>>>>>> 8121e969bc9c1a9e17f7e122b8a1d737db0b666f
     });
 
 });
