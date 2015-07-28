11410cd8c7b843ef71e81deb5f8ce238ae840aff
andrei <andreirtaylor@gmail.com>
2015-07-27 21:38:24 -0700

    Fixed socket duplication for real this time

--- Frontend/js/socketFactory.js
+++ Frontend/js/socketFactory.js
@@ -1,21 +1,23 @@
 (function(){
     // this is a factory that handles the socket events
-    // it is necessary to run the digest function on 
+    // it is necessary to run the digest function on
     // incoming data
     app.factory('socket', function ($rootScope) {
         var socket = io.connect();
-        // returns a object that will have the ability to 
+        // returns a object that will have the ability to
         // send and recieve data on the socket
         return {
             on: function (eventName, callback) {
-                if(!socket._callbacks[eventName]){;
-                    socket.on(eventName, function () {  
-                        var args = arguments;
-                        $rootScope.$apply(function () {
-                            callback.apply(socket, args);
-                        });
-                    });
+                // if there is already a listener, remove it
+                if(socket._callbacks[eventName]){;
+                    delete socket._callbacks[eventName];
                 }
+                socket.on(eventName, function () {
+                    var args = arguments;
+                    $rootScope.$apply(function () {
+                        callback.apply(socket, args);
+                    });
+                });
             },
             emit: function (eventName, data, callback) {
                 socket.emit(eventName, data, function () {
